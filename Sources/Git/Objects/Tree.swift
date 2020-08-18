4087d935f93f90a7f412365a58c36fe55206ff24
Mattt <mattt@me.com>
2020-08-18 12:33:07 -0700

    Conform Tree to RandomAccessCollection

--- Sources/Git/Objects/Tree.swift
+++ Sources/Git/Objects/Tree.swift
@@ -6,15 +6,13 @@ public final class Tree: Object {
 
     /// A tree entry.
     public final class Entry {
-        private(set) var tree: Tree
-        private(set) var index: Int
+        weak var tree: Tree?
 
-        var pointer: OpaquePointer {
-            git_tree_entry_byindex(tree.pointer, index)
-        }
+        var pointer: OpaquePointer
 
         /// The object corresponding to the tree entry.
         public var object: Object? {
+            guard let tree = tree else { return nil }
             var pointer: OpaquePointer?
             do {
                 try wrap { git_tree_entry_to_object(&pointer, tree.owner.pointer, self.pointer)}
@@ -35,19 +33,45 @@ public final class Tree: Object {
             return String(validatingUTF8: git_tree_entry_name(pointer))!
         }
 
-        init(in tree: Tree, at index: Int) {
+        required init(_ pointer: OpaquePointer) {
+            self.pointer = pointer
+        }
+
+        convenience init(in tree: Tree, at index: Int) {
+            self.init(git_tree_entry_byindex(tree.pointer, index))
             self.tree = tree
-            self.index = index
         }
     }
 
-    public var entries: [String: Entry] {
-        var entries: [String: Entry] = [:]
-        for index in 0..<git_tree_entrycount(pointer) {
-            let entry = Entry(in: self, at: index)
-            entries[entry.name] = entry
-        }
+    public subscript(_ name: String) -> Entry? {
+        return indices.lazy
+            .map { Entry(in: self, at: $0) }
+            .first(where: { $0.name == name })
+    }
+}
 
-        return entries
+// MARK: - Hashable
+
+extension Tree.Entry: Hashable {
+    public static func == (lhs: Tree.Entry, rhs: Tree.Entry) -> Bool {
+        (lhs.name, lhs.object?.id) == (rhs.name, rhs.object?.id)
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(object?.id)
+    }
+}
+
+// MARK: - RandomAccessCollection
+
+extension Tree: RandomAccessCollection {
+    public typealias Element = Entry
+
+    public var startIndex: Int { 0 }
+    public var endIndex: Int { git_tree_entrycount(pointer) }
+
+    public subscript(_ index: Int) -> Entry {
+        precondition(indices.contains(index))
+        return Entry(in: self, at: index)
     }
 }
