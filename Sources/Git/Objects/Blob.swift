d3bf0a88190b03bd653d308f0cd9071cb3bf0591
Mattt <mattt@me.com>
2020-08-19 23:54:29 -0700

    Implement additional properties on Blob

--- Sources/Git/Objects/Blob.swift
+++ Sources/Git/Objects/Blob.swift
@@ -5,9 +5,23 @@ import Foundation
 public final class Blob: Object {
     class override var type: git_object_t { return GIT_OBJECT_BLOB }
 
+    /// The repository containing the blob.
+    public override var owner: Repository {
+        return Repository(git_blob_owner(pointer))
+    }
+
+    /// Whether the blob is binary.
+    public var isBinary: Bool {
+        git_blob_is_binary(pointer) != 0
+    }
+
+    /// The size in bytes of the content
+    public var size: Int {
+        Int(git_blob_rawsize(pointer))
+    }
+
     /// The blob contents.
     public var data: Data {
-        let length = Int(git_blob_rawsize(pointer))
-        return Data(bytes: git_blob_rawcontent(pointer), count: length)
+        return Data(bytes: git_blob_rawcontent(pointer), count: size)
     }
 }
